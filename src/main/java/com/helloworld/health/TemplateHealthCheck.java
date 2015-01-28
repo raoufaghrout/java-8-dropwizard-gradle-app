8a05f858e71358f4b9528869d00b5aae17434761
Raouf Aghrout <aghrout.raouf@gmail.com>
2015-01-28 21:15:27 +0000

    Initial commit.

---
+++ src/main/java/com/helloworld/health/TemplateHealthCheck.java
@@ -0,0 +1,23 @@
+package com.helloworld.health;
+
+import com.codahale.metrics.health.HealthCheck;
+
+public class TemplateHealthCheck extends HealthCheck {
+
+    private final String template;
+
+    public TemplateHealthCheck(String template) {
+        this.template = template;
+    }
+
+    @Override
+    protected Result check() throws Exception {
+        final String saying = String.format(template, "Test");
+
+        if (!saying.contains("Test")) {
+            return Result.unhealthy("Template doesn't include a name!");
+        }
+
+        return Result.healthy();
+    }
+}
